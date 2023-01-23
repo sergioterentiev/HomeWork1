cfdf24976cfadb2f7c214a040c9424509a95b805
Serhii <sergio.terentiev@gmail.com>
2023-01-23 23:40:38 +0200

    HomeWork1

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -1,9 +1,10 @@
-public class HomeWork1 {
-    public static void main(String[] args) {
-        int sum = HomeWork1.sumOfTwoNumbers(1, 2);
-        System.out.println(sum);
-    }
-    public static int sumOfTwoNumbers(int a, int b){
-        return a+b;
-    }
+викладачу: class myClass {
+    int Year = 2023;
+    public static int MYMETHOD(int a,
+                               int b){
+        int c = a + b; //sum of 2 numbers
+        return c;}
+    public static void main(String[]
+                                    args) {
+        MYMETHOD(num1, num2);}
 }
