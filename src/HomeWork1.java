d12d8c8c214ec32949a1b81a08bd0a0bcb243af9
Serhii <sergio.terentiev@gmail.com>
2023-01-23 20:10:11 +0200

    HW1

---
+++ src/HomeWork1.java
@@ -0,0 +1,9 @@
+class HomeWork1 {
+    public static void main(String[] args) {
+        int sum = HomeWork1.sumOfTwoNumbers(1, 2);
+        System.out.println(sum);
+    }
+    public static int sumOfTwoNumbers(int a, int b){
+        return a+b;
+    }
+}
